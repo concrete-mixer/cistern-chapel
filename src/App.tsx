fa108243889c12571a8ccfeb635f2851dc624762
Stuart McDonald <stuartcmcd@gmail.com>
2021-10-01 16:17:43 +1300

    Make app mobile friendlier

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,63 @@ import styles from "./App.module.css";
 const App: React.FC = (): React.ReactElement => {
     const [playerState, setPlayerState] = React.useState("stopped");
     return (
-        <div className={styles.backgroundWrapper}>
-            <div className={styles.backgroundShade}>
-                <div className={styles.boxWrapper}>
-                    <div className={styles.box}>
-                        <h1>Welcome to the Cistern Chapel</h1>
-                        <p>You can join us in aural communion by clicking the &quot;Play audio&quot; button below.</p>
-                        {playerState === "stopped" && (
-                            <button
-                                onClick={() => {
-                                    setPlayerState("loading");
-                                    start(() => setPlayerState("playing"));
-                                }}
-                            >
-                                Play audio
-                            </button>
-                        )}
-                        {playerState === "loading" && <p>Loading files...</p>}
-                        {playerState === "playing" && (
-                            <button
-                                onClick={() => {
-                                    setPlayerState("stopped");
-                                    stop();
-                                }}
-                            >
-                                Stop audio
-                            </button>
-                        )}
-                        <div className={styles.explanation}>
-                            <h2>Explanation</h2>
-                            <p>
-                                This is a web-based implementation of Flush Tones, an as-yet unrealised sound art
-                                installation.
-                            </p>
-                            <p>
-                                The idea is to make public toilets less intimidating by filling them up with strange
-                                sounds to mask the real ones.{" "}
-                                <a href="https://en.wikipedia.org/wiki/Paruresis">Paruresis</a> and{" "}
-                                <a href="https://en.wikipedia.org/wiki/Parcopresis">Parcopresis</a> are real conditions,
-                                people!
-                            </p>
-                            <p>
-                                So far I&apos;ve had trouble finding a bathroom with power sockets to install speakers
-                                in. Or finding an organisation willing to have their bathrooms haunted. One day, maybe.
-                                For now, punters can meditate in the Cistern Chapel with smartphones and earphones.
-                            </p>
-                            <p>Plop.</p>
+        <div>
+            <div className={styles.backgroundWrapper}>
+                <div className={styles.backgroundShade}></div>
+            </div>
+            <div className={styles.boxWrapper}>
+                <div className={styles.box}>
+                    <h1>Welcome to the Cistern Chapel</h1>
+                    <p>You can join us in aural communion by clicking the &quot;Play audio&quot; button below.</p>
+                    {playerState === "stopped" && (
+                        <button
+                            onClick={() => {
+                                setPlayerState("loading");
+                                start(() => setPlayerState("playing"));
+                            }}
+                        >
+                            Play audio
+                        </button>
+                    )}
+                    {playerState === "loading" && <button>Loading files...</button>}
+                    {playerState === "playing" && (
+                        <button
+                            onClick={() => {
+                                setPlayerState("stopped");
+                                stop();
+                            }}
+                        >
+                            Stop audio
+                        </button>
+                    )}
+                    <div className={styles.explanation}>
+                        <h2>Explanation</h2>
+                        <p>
+                            This is a web-based implementation of Flush Tones, an as-yet unrealised sound art
+                            installation.
+                        </p>
+                        <p>
+                            The idea is to make public toilets less intimidating by filling them up with strange sounds
+                            to mask the real ones. <a href="https://en.wikipedia.org/wiki/Paruresis">Paruresis</a> and{" "}
+                            <a href="https://en.wikipedia.org/wiki/Parcopresis">Parcopresis</a> are real conditions,
+                            people!
+                        </p>
+                        <p>
+                            So far I&apos;ve had trouble finding a bathroom with power sockets to install speakers in.
+                            Or finding an organisation willing to have their bathrooms haunted. One day, maybe.
+                        </p>
+                        <p>
+                            <a href="mailto:concretemixer.audio@gmail.com">Sales and contact</a>
+                        </p>
+                        <div className={styles.backgroundAttribution}>
+                            <a href="https://commons.wikimedia.org/wiki/File:Basilica_Cistern_Istanbul.JPG">
+                                Background photo
+                            </a>{" "}
+                            of <a href="https://en.wikipedia.org/wiki/Basilica_Cistern">Basilica Cistern, Istanbul</a>{" "}
+                            by Moise Nicu, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia
+                            Commons
                         </div>
                     </div>
-                </div>
-                <div className={styles.backgroundAttribution}>
-                    Photo of <a href="https://en.wikipedia.org/wiki/Basilica_Cistern">Basilica Cistern, Istanbul</a> by
-                    Moise Nicu, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia Commons
                 </div>
             </div>
         </div>
